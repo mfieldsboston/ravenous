af1ff57e7db1b7cd5c5d34ee53e352d9d5c342f0
mfieldsboston <mfields11@gmail.com>
2017-11-12 15:49:21 -0500

    ravenous 4 to resolve

--- src/App.js
+++ src/App.js
@@ -2,8 +2,10 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import BusinessList from './components/BusinessList/BusinessList';
-import SearchBar from './components/SearchBar/SearchBar'
+import SearchBar from './components/SearchBar/SearchBar';
+//import SearchBar from './util/Yelp';
 
+//Help needed below for soing away with business and businesses
 let business = {
   imageSrc: 'https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg',
   name: 'MarginOtto Pizzeria',
@@ -24,10 +26,24 @@ const businesses = [
   business,
   business
 ];
+//Help needed above for soing away with business and businesses
+
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      businesses: []
+    };
+    this.searchYelp.bind(this);
+  }
   searchYelp(term, location, sortBy) {
-    console.log('Searching Yelp with ' + term + location + sortBy);
+    //console.log('Searching Yelp with ' + term + location + sortBy);
+    // Help needed below
+    /*Yelp.search(term, location, sortBy).then(businesses => {
+          this.setState({ bustnesses: businesses });
+    });*/
+    // Help needed above
   }
   render() {
     return (
---
+++ src/util/Yelp_assist.js
@@ -0,0 +1,56 @@
+const clientId = 'WZyPW32NqyJNocfn2y1KjA';
+const secret = 'UQDA4nJqnKT0Qbw7dpGulvEwQ1WiizT2JP9K9JBK3FcZZ1bmokfjxT7GVxy5u01E';
+
+let accessToken = '';
+
+let Yelp = {
+
+  getAccessToken() {
+    if (accessToken) { return new Promise(resolve => resolve(accessToken)); }
+    return fetch(
+      `https://cors-anywhere.herokuapp.com/https://api.yelp.com/oauth2/token?grant_type=client_credentials&client_id=${clientId}&client_secret=${secret}`,
+      {method: 'POST'}
+    ).then(
+      response => { return response.json() }
+    ).then(
+      jsonResponse => accessToken = jsonResponse.access_token
+    )
+  },
+
+  search(term, location, sortBy) {
+    return Yelp.getAccessToken().then(
+      () => {
+        return fetch(
+          `https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`,
+          { headers: { Authorization: `Bearer ${accessToken}` } }
+        ).then(
+          response => { return response.json() }
+        ).then(
+          jsonResponse => {
+            if ( jsonResponse.businesses ) {
+              return jsonResponse.businesses.map(
+                business => {
+                  return {
+                    id: business.id,
+                    imageSrc: business.image_url,
+                    name: business.name,
+                    address: business.location.address1,
+                    city: business.location.city,
+                    state: business.location.state,
+                    zipCode: business.location.zip_code,
+                    category: business.categories[0].title,
+                    rating: business.rating,
+                    reviewCount: business.review_count,
+                  }
+                }
+              )
+            }
+          }
+        )
+      }
+    )
+  }
+
+};
+
+module.exports = Yelp;
